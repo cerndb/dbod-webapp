8e60e90c932fe835154e5003f472fa0acbe566ab
dgomezbl <dgomezbl@4525493e-7705-40b1-a816-d608a930855b>
2012-01-26 11:08:52 +0000

    Upgrade functionality
    git-svn-id: https://svn.cern.ch/reps/dbondemand@137 4525493e-7705-40b1-a816-d608a930855b

--- DBOnDemand/src/java/ch/cern/dod/ui/controller/AdminController.java
+++ DBOnDemand/src/java/ch/cern/dod/ui/controller/AdminController.java
@@ -1,9 +1,13 @@
 package ch.cern.dod.ui.controller;
 
 import ch.cern.dod.db.dao.DODInstanceDAO;
+import ch.cern.dod.db.dao.DODUpgradeDAO;
 import ch.cern.dod.db.entity.DODInstance;
+import ch.cern.dod.db.entity.DODUpgrade;
 import ch.cern.dod.ui.model.InstanceListModel;
+import ch.cern.dod.ui.model.UpgradesListModel;
 import ch.cern.dod.ui.renderer.OverviewGridRenderer;
+import ch.cern.dod.ui.renderer.UpgradesGridRenderer;
 import ch.cern.dod.util.DODConstants;
 import ch.cern.dod.util.JobHelper;
 import java.util.ArrayList;
@@ -26,9 +30,16 @@ import org.zkoss.zul.Window;
 /**
  * Controller for the admin overview of instances. It allows the admins to manage every instance.
  * @author Daniel Gomez Blanco
- * @version 22/11/2011
  */
 public class AdminController extends Vbox implements BeforeCompose, AfterCompose{
+    /**
+     * Upgrade DAO
+     */
+    private DODUpgradeDAO upgradeDAO;
+    /**
+     * Instance DAO
+     */
+    private DODInstanceDAO instanceDAO;
     /**
      * List of instances. In this case, all the instances in the database.
      */
@@ -41,7 +52,14 @@ public class AdminController extends Vbox implements BeforeCompose, AfterCompose
      * Instances checked on the grid.
      */
     private ArrayList<DODInstance> checked;
-    
+    /**
+     * List of upgrades.
+     */
+    private List<DODUpgrade> upgrades;
+    /**
+     * Number of upgrades, defining a public method to show the grid or not.
+     */
+    private int upgradesSize;
     /**
      * Job helper to perform admin actions.
      */
@@ -59,11 +77,22 @@ public class AdminController extends Vbox implements BeforeCompose, AfterCompose
         username = execution.getHeader(DODConstants.ADFS_LOGIN);
         jobHelper = new JobHelper(true);
         checked = new ArrayList<DODInstance>();
-        instances = new DODInstanceDAO().selectAll();
+        
+        //Select upgrades
+        upgradeDAO = new DODUpgradeDAO();
+        upgrades = upgradeDAO.selectAll();
+        if (upgrades != null)
+            upgradesSize = upgrades.size();
+        else
+            upgradesSize = 0;
+        //Select instances
+        instanceDAO = new DODInstanceDAO();
+        instances = instanceDAO.selectAll(upgrades);
         if (instances != null)
             instancesSize = instances.size();
         else
             instancesSize = 0;
+        
     }
 
     /**
@@ -72,10 +101,16 @@ public class AdminController extends Vbox implements BeforeCompose, AfterCompose
      */
     public void afterCompose() {
         if (instancesSize > 0) {
-            Grid grid = (Grid) getFellow("overviewGrid");
-            grid.setModel(new InstanceListModel(instances));
-            grid.setRowRenderer(new OverviewGridRenderer(checked));
-            grid.getPagingChild().setMold("os");
+            Grid overviewGrid = (Grid) getFellow("overviewGrid");
+            overviewGrid.setModel(new InstanceListModel(instances));
+            overviewGrid.setRowRenderer(new OverviewGridRenderer(checked));
+            overviewGrid.getPagingChild().setMold("os");
+        }
+        if (upgradesSize > 0) {
+            Grid upgradesGrid = (Grid) getFellow("upgradesGrid");
+            upgradesGrid.setModel(new UpgradesListModel(upgrades));
+            upgradesGrid.setRowRenderer(new UpgradesGridRenderer(upgradeDAO));
+            upgradesGrid.getPagingChild().setMold("os");
         }
     }
 
@@ -83,8 +118,15 @@ public class AdminController extends Vbox implements BeforeCompose, AfterCompose
      * Refreshes the list of instances.
      */
     public void refreshInstances(){
+        //Get upgrades
+        upgrades = upgradeDAO.selectAll();
+        if (upgrades != null)
+            upgradesSize = upgrades.size();
+        else
+            upgradesSize = 0;
+        
         //Get instances
-        instances = new DODInstanceDAO().selectAll();
+        instances = instanceDAO.selectAll(upgrades);
         if (instances != null)
             instancesSize = instances.size();
         else
@@ -98,9 +140,13 @@ public class AdminController extends Vbox implements BeforeCompose, AfterCompose
             }
         }
         
+        //Set the new upgrades
+        Grid upgradesGrid = (Grid) getFellow("upgradesGrid");
+        ((UpgradesListModel)upgradesGrid.getModel()).setUpgrades(upgrades);
+        
         //Set the new instances
-        Grid grid = (Grid) getFellow("overviewGrid");
-        ((InstanceListModel)grid.getModel()).setInstances(instances);
+        Grid overviewGrid = (Grid) getFellow("overviewGrid");
+        ((InstanceListModel)overviewGrid.getModel()).setInstances(instances);
     }
 
     /**
@@ -109,6 +155,14 @@ public class AdminController extends Vbox implements BeforeCompose, AfterCompose
      */
     public int getInstancesSize() {
         return instancesSize;
+    }
+    
+    /**
+     * Getter for the upgrades size.
+     * @return number of upgrades.
+     */
+    public int getUpgradesSize() {
+        return upgradesSize;
     }
     
     /**
@@ -127,12 +181,14 @@ public class AdminController extends Vbox implements BeforeCompose, AfterCompose
             ((Toolbarbutton) getFellow("startupAllBtn")).setDisabled(false);
             ((Toolbarbutton) getFellow("shutdownAllBtn")).setDisabled(false);
             ((Toolbarbutton) getFellow("backupAllBtn")).setDisabled(false);
+            ((Toolbarbutton) getFellow("upgradeAllBtn")).setDisabled(false);
         }
         else {
             checked.removeAll(checked);
             ((Toolbarbutton) getFellow("startupAllBtn")).setDisabled(true);
             ((Toolbarbutton) getFellow("shutdownAllBtn")).setDisabled(true);
             ((Toolbarbutton) getFellow("backupAllBtn")).setDisabled(true);
+            ((Toolbarbutton) getFellow("upgradeAllBtn")).setDisabled(true);
         }
         
         //Re-render the list
@@ -141,14 +197,14 @@ public class AdminController extends Vbox implements BeforeCompose, AfterCompose
     }
     
     /**
-     * Startup all selected instances. It checks that instances are not awaiting approval.
+     * Startup all selected instances. It checks that instances are not awaiting approval or running.
      * It does not matter if the instance is pending a job, it will be executed after it.
      */
     public void startupAll() {
         boolean error = false;
         for (int i=0; i<checked.size(); i++) {
             DODInstance instance = checked.get(i);
-            if (!instance.getState().equals(DODConstants.INSTANCE_STATE_AWAITING_APPROVAL)) {
+            if (!instance.getState().equals(DODConstants.INSTANCE_STATE_AWAITING_APPROVAL) && !instance.getState().equals(DODConstants.INSTANCE_STATE_RUNNING)) {
                 if (!jobHelper.doStartup(instance, username)){
                     error = true;
                 }
@@ -163,14 +219,14 @@ public class AdminController extends Vbox implements BeforeCompose, AfterCompose
     }
     
     /**
-     * Shutdown all selected instances. It checks that instances are not awaiting approval.
+     * Shutdown all selected instances. It checks that instances are not awaiting approval or stopped.
      * It does not matter if the instance is pending a job, it will be executed after it.
      */
     public void shutdownAll() {
         boolean error = false;
         for (int i=0; i<checked.size(); i++) {
             DODInstance instance = checked.get(i);
-            if (!instance.getState().equals(DODConstants.INSTANCE_STATE_AWAITING_APPROVAL)) {
+            if (!instance.getState().equals(DODConstants.INSTANCE_STATE_AWAITING_APPROVAL) && !instance.getState().equals(DODConstants.INSTANCE_STATE_STOPPED)) {
                 if (!jobHelper.doShutdown(instance, username)){
                     error = true;
                 }
@@ -204,6 +260,45 @@ public class AdminController extends Vbox implements BeforeCompose, AfterCompose
         //Show error if any
         if (error)
             showError(DODConstants.ERROR_COLLECTIVE_ACTION);
+    }
+    
+    /**
+     * Upgrade all selected instances. It checks that instances are not awaiting approval and have a pending upgrade.
+     * It does not matter if the instance is pending a job, it will be executed after it.
+     */
+    public void upgradeAll(){
+        boolean error = false;
+        for (int i=0; i<checked.size(); i++) {
+            DODInstance instance = checked.get(i);
+            if (!instance.getState().equals(DODConstants.INSTANCE_STATE_AWAITING_APPROVAL) && instance.getVersion() != null && !instance.getVersion().isEmpty()) {
+                if (!jobHelper.doUpgrade(instance, username)){
+                    error = true;
+                }
+            }
+        }
+        //Re-render the list
+        Grid grid = (Grid) getFellow("overviewGrid");
+        ((InstanceListModel)grid.getModel()).setInstances(instances);
+        //Show error if any
+        if (error)
+            showError(DODConstants.ERROR_COLLECTIVE_ACTION);
+    }
+    
+    /**
+     * Opens the add upgrade window.
+     */
+    public void openAddUpgrade(){
+        try {
+            AddUpgradeController upgradeController = new AddUpgradeController();
+            //Only show window if it is not already being diplayed
+            if (getFellowIfAny(upgradeController.getId()) == null) {
+                upgradeController.setParent(this);
+                upgradeController.doModal();
+            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(AdminController.class.getName()).log(Level.SEVERE, "ERROR OPENING ADD UPGRADE WINDOW", ex);
+            showError(DODConstants.ERROR_OPENING_ADD_UPGRADE);
+        }
     }
     
     /**
