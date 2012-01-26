76dacbd60db5d340b59bedb81a5bda7a473ae1b2
dgomezbl <dgomezbl@4525493e-7705-40b1-a816-d608a930855b>
2012-01-26 11:07:58 +0000

    Upgrade functionality
    git-svn-id: https://svn.cern.ch/reps/dbondemand@135 4525493e-7705-40b1-a816-d608a930855b

---
+++ DBOnDemand/src/java/ch/cern/dod/db/entity/DODUpgrade.java
@@ -0,0 +1,67 @@
+package ch.cern.dod.db.entity;
+
+/**
+ * Represents an upgrade to be added to the system.
+ * @author Daniel Gomez Blanco
+ */
+public class DODUpgrade {
+    /**
+     * Database type (Oracle, MySQL, etc)
+     */
+   private String dbType;
+   /**
+     * Category of the instances (personal, test or official, max. 32)
+     */
+   private String category;
+   /**
+    * Version to which the update is from
+    */
+   private String versionFrom;
+   /**
+    * Version to which the update is to
+    */
+   private String versionTo;
+
+    public String getDbType() {
+        return dbType;
+    }
+
+    public void setDbType(String dbType) {
+        this.dbType = dbType;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getVersionFrom() {
+        return versionFrom;
+    }
+
+    public void setVersionFrom(String versionFrom) {
+        this.versionFrom = versionFrom;
+    }
+
+    public String getVersionTo() {
+        return versionTo;
+    }
+
+    public void setVersionTo(String versionTo) {
+        this.versionTo = versionTo;
+    }
+    
+    public int compareTo(Object object) {
+        DODUpgrade upgrade = (DODUpgrade) object;
+        return this.getDbType().compareTo(upgrade.getDbType());
+    }
+    
+    @Override
+    public boolean equals(Object object) {
+        DODUpgrade upgrade = (DODUpgrade) object;
+        return this.getDbType().equals(upgrade.getDbType()) && this.getCategory().equals(upgrade.getCategory()) && this.getVersionFrom().equals(upgrade.getVersionFrom());
+    }
+}
