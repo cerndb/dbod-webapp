af900b9685cb13ad87decf18a6a17db80095d9c8
icoteril <icoteril@4525493e-7705-40b1-a816-d608a930855b>
2012-05-18 15:21:04 +0000

    Cleaned Oracle.pm
    git-svn-id: https://svn.cern.ch/reps/dbondemand@225 4525493e-7705-40b1-a816-d608a930855b

--- dod_daemon/tags/release-1.1/lib/DOD/Oracle.pm
+++ dod_daemon/tags/release-1.1/lib/DOD/Oracle.pm
@@ -9,14 +9,7 @@ use File::ShareDir;
 use Log::Log4perl;
 use File::Temp;
 
-use DBI;
-use DBD::Oracle qw(:ora_types);
-use POSIX qw(strftime);
-
-use DOD::ConfigParser;
-
-our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS, $config, $config_dir, $logger,
-    $DSN, $DBTAG, $DATEFORMAT, $user, $password);
+our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS, $config, $config_dir, $logger);
 
 $VERSION     = 0.03;
 @ISA         = qw(Exporter);
@@ -28,23 +21,6 @@ $VERSION     = 0.03;
 
 BEGIN{
 
-sub getPassword 
-{
-    my ($tag, $password_file) = @_;
-    my @passwd_lines;
-    $logger->debug("password_file = $password_file, tag= $tag");
-    open (PASS, $password_file) && (defined $tag) or return;
-    @passwd_lines = <PASS>;
-    close PASS;
-
-    my @line = grep(/^\s*TAG\s+$tag=/, @passwd_lines);
-    if ((defined $line[0]) && ($line[0] =~ m/^\s*TAG\s+$tag=(.*)/))
-    {
-        return $1;
-    }
-    return undef;
-}
-    
 $config_dir = File::ShareDir::dist_dir( "DOD" );
 $config = LoadFile( "$config_dir/dod.conf" );
 Log::Log4perl::init( "$config_dir/$config->{'LOGGER_CONFIG'}" );
@@ -55,23 +31,6 @@ foreach my $key ( keys(%{$config}) ) {
     my %h = %{$config};
     $logger->debug( "\t$key -> $h{$key}" );
     }
-
-# DB configuration parameters
-$DSN = $config->{'DB_DSN'} ;
-$DBTAG = $config->{'DB_USER'};
-$DATEFORMAT = $config->{'DB_DATE_FORMAT'};
-my @buf = split( /_/, $DBTAG );
-$user = pop( @buf );
-$password = getPassword( $DBTAG, $config->{'PASSWORD_FILE'} );
-if (defined $password){
-    $logger->debug( "Testing database connection for $user:$DSN:XXXXXXXX" );
-    my $dbh = DBI->connect( $DSN, $user, $password) ;
-    $logger->debug( "Disconnecting" );
-    $dbh->disconnect();
-    }
-else {
-    $logger->error_die("Check DB connection parameters. Couldn't start a connection\n $!" );
-}
 
 } # BEGIN BLOCK
 
