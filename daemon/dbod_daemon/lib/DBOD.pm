ab0f67d6ea4471b113da1f8b2d5321798d696adc
Database On Demand <dbod@dbvrtg4011.cern.ch>
2013-04-15 16:44:31 +0200

    Substituting references to DOD for DBOD in core modules

--- daemon/dbod_daemon/lib/DBOD.pm
+++ daemon/dbod_daemon/lib/DBOD.pm
@@ -1,4 +1,4 @@
-package DOD;
+package DBOD;
 
 use strict;
 use warnings;
@@ -8,11 +8,11 @@ use DBI;
 use DBD::Oracle qw(:ora_types);
 use POSIX qw(strftime);
 
-use DOD::Config qw( $config );
-use DOD::Database;
-use DOD::MySQL;
-use DOD::Oracle;
-use DOD::All;
+use DBOD::Config qw( $config );
+use DBOD::Database;
+use DBOD::MySQL;
+use DBOD::Oracle;
+use DBOD::All;
 
 use POSIX ":sys_wait_h";
 
@@ -28,24 +28,24 @@ $VERSION     = 0.03;
 # Load general configuration
 
 INIT {
-    $logger = Log::Log4perl::get_logger( 'DOD' );
+    $logger = Log::Log4perl::get_logger( 'DBOD' );
     $logger->debug( "Logger created" );
 } # BEGIN BLOCK
 
 my %command_callback_table = (
-    'UPGRADE' => { 'MYSQL' => \&DOD::MySQL::upgrade_callback , 
-                    'ORACLE' => \&DOD::Oracle::upgrade_callback }
+    'UPGRADE' => { 'MYSQL' => \&DBOD::MySQL::upgrade_callback , 
+                    'ORACLE' => \&DBOD::Oracle::upgrade_callback }
 );
 
 my %state_checker_table = (
-    'MYSQL' => \&DOD::MySQL::state_checker,
-    'ORACLE' => \&DOD::Oracle::state_checker 
+    'MYSQL' => \&DBOD::MySQL::state_checker,
+    'ORACLE' => \&DBOD::Oracle::state_checker 
 );
 
 sub jobDispatcher {
     # This is neccesary because daemonizing closes all file descriptors
-    my $logger = Log::Log4perl::get_logger( "DOD.jobDispatcher" );
-    my $dbh = DOD::Database::getDBH();
+    my $logger = Log::Log4perl::get_logger( "DBOD.jobDispatcher" );
+    my $dbh = DBOD::Database::getDBH();
     my @tasks;
     my @job_list;
     while (1){
@@ -55,17 +55,17 @@ sub jobDispatcher {
             $logger->error("The connecion to the DB was lost");
             $dbh = undef;
             $logger->debug("Creating new DB connection");
-            $dbh = DOD::Database::getDBH();
+            $dbh = DBOD::Database::getDBH();
         }
         
         $logger->debug( "Fetching job list" );
-        push(@job_list, DOD::Database::getJobList($dbh));
+        push(@job_list, DBOD::Database::getJobList($dbh));
         my $pendingjobs = $#job_list + 1;
         $logger->debug( "Pending jobs: $pendingjobs" );
         if ($pendingjobs > 0){
             foreach my $job (@job_list){
                 $logger->debug( sprintf("Fetching job params" ) );
-                $job->{'PARAMS'} = DOD::Database::getJobParams($job, $dbh);
+                $job->{'PARAMS'} = DBOD::Database::getJobParams($job, $dbh);
                 $logger->debug( sprintf("Number of open tasks: %d", $#tasks + 1) );
                 if ($#tasks < 20){
                     my $worker_pid = fork();
@@ -78,7 +78,7 @@ sub jobDispatcher {
                         $task->{'job'} = $job;
                         push(@tasks, $task);
                         # Updates job status to RUNNING
-                        DOD::Database::updateJob($job, 'STATE', 'RUNNING', $dbh); # forking destroys $dbh ??
+                        DBOD::Database::updateJob($job, 'STATE', 'RUNNING', $dbh); # forking destroys $dbh ??
                     }
                     else{
                         &worker_body($job, $dbh);
@@ -99,15 +99,15 @@ sub jobDispatcher {
             # Cleaning stranded jobs 
             $logger->debug( "No pending jobs" );
             $logger->debug( "Checking for timed out jobs" );
-            my @timedoutjobs = DOD::Database::getTimedOutJobs($dbh);
+            my @timedoutjobs = DBOD::Database::getTimedOutJobs($dbh);
             foreach my $job (@timedoutjobs){
                 my $state_checker = get_state_checker($job);
                 if (! defined($state_checker)){
                     $logger->error( "Not state checker defined for this DB type" );
                 }
                 my ($job_state, $instance_state) = $state_checker->($job, 1);
-                DOD::Database::finishJob( $job, $job_state, "TIMED OUT", $dbh );
-                DOD::Database::updateInstance( $job, 'STATE', $instance_state, $dbh );
+                DBOD::Database::finishJob( $job, $job_state, "TIMED OUT", $dbh );
+                DBOD::Database::updateInstance( $job, 'STATE', $instance_state, $dbh );
                 my $task = $job->{'task'};
                 if (ref $task) {
                     my $pid = $task->{'pid'};
@@ -147,41 +147,41 @@ sub jobDispatcher {
 
 sub worker_body {
     my ($job, $pdbh) = @_;
-    my $logger = Log::Log4perl::get_logger( "DOD.worker" );
+    my $logger = Log::Log4perl::get_logger( "DBOD.worker" );
 
     my $worker_dbh;
     if (defined($pdbh)){
         # Cloning parent process DB handler
         $logger->debug("Cloning parent DB handler");
         $worker_dbh = $pdbh->clone();
-        $logger->debug( "Setting date format: $DOD::Database::DATEFORMAT" );
-        $worker_dbh->do( "alter session set NLS_DATE_FORMAT='$DOD::Database::DATEFORMAT'" );
+        $logger->debug( "Setting date format: $DBOD::Database::DATEFORMAT" );
+        $worker_dbh->do( "alter session set NLS_DATE_FORMAT='$DBOD::Database::DATEFORMAT'" );
         $pdbh->{InactiveDestroy} = 1;
         undef $pdbh;
     }
     else{
         # Obtaining a new DB connector
-        $worker_dbh = DOD::Database::getDBH();
+        $worker_dbh = DBOD::Database::getDBH();
     }
 
-    my $cmd_line = DOD::Database::prepareCommand($job, $worker_dbh);
+    my $cmd_line = DBOD::Database::prepareCommand($job, $worker_dbh);
     $logger->debug( "Received cmd_line: $cmd_line ");
     my $log;
     my $retcode;
     my $params = $job->{'PARAMS'};
     if (defined $cmd_line){
-        my $entity = DOD::All::get_entity($job);
+        my $entity = DBOD::All::get_entity($job);
         my $cmd =  "/etc/init.d/syscontrol -i $entity $cmd_line";
         $logger->debug( "Executing $cmd" );
         $log = `$cmd`;
-        $retcode = DOD::All::result_code($log);
+        $retcode = DBOD::All::result_code($log);
         $logger->debug( "Finishing Job. Return code: $retcode" );
-        DOD::Database::finishJob( $job, $retcode, $log, $worker_dbh );
+        DBOD::Database::finishJob( $job, $retcode, $log, $worker_dbh );
     }
     else{
         $logger->error( "An error ocurred preparing command execution \n $!" );
         $logger->debug( "Finishing Job.");
-        DOD::Database::finishJob( $job, 1, $!, $worker_dbh );
+        DBOD::Database::finishJob( $job, 1, $!, $worker_dbh );
     }
     $logger->debug( "Exiting worker process" );
     $worker_dbh->disconnect();
