050280994007cf26ecec485877a71faa02013937
dgomezbl <dgomezbl@PCITDES22.cern.ch>
2013-05-28 10:56:10 +0200

    Fixed slowness downloading log files Issue #DBOD-59 - Slowness downloading log files

--- webapp/DBOnDemand/src/java/ch/cern/dod/util/FileHelper.java
+++ webapp/DBOnDemand/src/java/ch/cern/dod/util/FileHelper.java
@@ -3,8 +3,6 @@ package ch.cern.dod.util;
 import ch.cern.dod.db.entity.DODInstance;
 import ch.cern.dod.ws.DODWebService;
 import ch.cern.dod.ws.DODWebServicePortType;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -125,14 +123,7 @@ public class FileHelper {
         AMedia file = null;
         try {
            URL url = new URL(urlString);
-           BufferedReader rd = new BufferedReader(new InputStreamReader(url.openStream()));
-           String line;
-           String content = "";
-           while ((line = rd.readLine()) != null) {
-              content += line + "\n";
-           }
-           rd.close();
-           file = new AMedia(filePath.substring(filePath.lastIndexOf("/") + 1), null, "text/plain", content);
+           file = new AMedia(filePath.substring(filePath.lastIndexOf("/") + 1), null, "text/plain", url, "UTF-8");
         } catch (Exception ex) {
            Logger.getLogger(FileHelper.class.getName()).log(Level.SEVERE, "ERROR SERVING FILE ON INSTANCE " + instance.getDbName(), ex.getMessage());
         }
