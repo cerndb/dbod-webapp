e6a0ab23f56951a28a203c6f21b88220b581149b
José Andrés <jacb_667@hotmail.com>
2015-10-14 09:58:39 +0200

    Added LICENSE and COPYRIGHT. Added headers to source files.

--- src/main/java/ch/cern/dbod/db/dao/UpgradeDAO.java
+++ src/main/java/ch/cern/dbod/db/dao/UpgradeDAO.java
@@ -1,7 +1,12 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2015, CERN
+ * This software is distributed under the terms of the GNU General Public
+ * Licence version 3 (GPL Version 3), copied verbatim in the file "LICENSE".
+ * In applying this license, CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as Intergovernmental Organization
+ * or submit itself to any jurisdiction.
  */
+ 
 package ch.cern.dbod.db.dao;
 
 import ch.cern.dbod.db.entity.Upgrade;
