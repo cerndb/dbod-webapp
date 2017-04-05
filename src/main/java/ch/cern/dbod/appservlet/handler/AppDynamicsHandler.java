0cbc2a50f4cf8c69fad8279c77813b1f5cb459a7
José Andrés Cordero Benítez <j.cordero@cern.ch>
2017-04-05 11:39:03 +0200

    Some refactor in AppDynamics Proxy Handler.

--- src/main/java/ch/cern/dbod/appservlet/handler/AppDynamicsHandler.java
+++ src/main/java/ch/cern/dbod/appservlet/handler/AppDynamicsHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015, CERN
+ * Copyright (C) 2017, CERN
  * This software is distributed under the terms of the GNU General Public
  * Licence version 3 (GPL Version 3), copied verbatim in the file "LICENSE".
  * In applying this license, CERN does not waive the privileges and immunities
@@ -7,24 +7,23 @@
  * or submit itself to any jurisdiction.
  */
 
-package ch.cern.dbod.appservlet;
+package ch.cern.dbod.appservlet.handler;
 
-import ch.cern.dbod.util.ConfigLoader;
 import ch.cern.dbod.util.CommonConstants;
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import ch.cern.dbod.util.ConfigLoader;
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
@@ -35,12 +34,15 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
 
 /**
- * HttpConnection class to communicate with the AppDynamics application.
+ * Parser implementation for AppDynamics.
  * @author Jose Andres Cordero Benitez
- * June 2015
+ * April 2017
  */
-public class HttpConnection {
+public class AppDynamicsHandler {
     
+    public static final int TOKEN_STATUS_OK = 0;
+    public static final int TOKEN_STATUS_FAIL = 1;
+    public static final int TOKEN_STATUS_NEW = 2;
     private static final Map<String, String> SESSIONS = new HashMap<>();
     
     // Save the last day of created sessions. When this value changes, all the sessions will be cleared.
@@ -118,7 +120,7 @@ public class HttpConnection {
      * @param mimetype Mimetype of the current code, to parse correctly
      * @throws IOException 
      */
-    public static void parseAndReplaceCode(String contextPath, BufferedReader rd, PrintWriter out, String mimetype) throws IOException
+    private static void parseAndReplaceCode(String contextPath, BufferedReader rd, PrintWriter out, String mimetype) throws IOException
     {
         String line;
         if (mimetype != null && (mimetype.endsWith("javascript") || mimetype.endsWith("css")))
@@ -250,18 +252,18 @@ public class HttpConnection {
             checkSessions();
             
             // Get sessionid from received Cookies
-            String appdynid = ProxyUtils.getSessionFromCookies(request);
+            String appdynid = getSessionFromCookies(request);
             
             HttpGet httprequest = new HttpGet(url);
             
             // Do all the security checks
-            int token_result = ProxyUtils.checkSecurityToken(request, appdynid, httprequest, SESSIONS);
+            int token_result = checkSecurityToken(request, appdynid, httprequest, SESSIONS);
             switch(token_result)
             {
-                case ProxyUtils.TOKEN_STATUS_FAIL:
+                case TOKEN_STATUS_FAIL:
                     response.sendError(403);
                     return;
-                case ProxyUtils.TOKEN_STATUS_NEW:
+                case TOKEN_STATUS_NEW:
                     appdynid = null;
             }
             
@@ -277,7 +279,7 @@ public class HttpConnection {
             {
                 // Get all the cookies and save them
                 if (appdynid == null)
-                    ProxyUtils.getAndProcessCookies(httpresponse, response, SESSIONS);
+                    getAndProcessCookies(httpresponse, response, SESSIONS);
             }
             
             // Get the reader of the received page and the writer for the output
@@ -290,7 +292,7 @@ public class HttpConnection {
         }
         catch(IOException | IllegalStateException ex)
         {
-            Logger.getLogger(HttpConnection.class.getName()).log(Level.SEVERE, "UNCAUGHT EXCEPTION", ex);
+            Logger.getLogger(AppDynamicsHandler.class.getName()).log(Level.SEVERE, "UNCAUGHT EXCEPTION", ex);
         }
     }
     
@@ -325,7 +327,7 @@ public class HttpConnection {
         }
         catch(IOException | IllegalStateException ex)
         {
-            Logger.getLogger(HttpConnection.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(AppDynamicsHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
@@ -354,7 +356,7 @@ public class HttpConnection {
             }
         }
         catch(IOException | IllegalStateException ex) {
-            Logger.getLogger(HttpConnection.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(AppDynamicsHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
@@ -402,7 +404,147 @@ public class HttpConnection {
         }
         catch (IOException ex)
         {
-            Logger.getLogger(HttpConnection.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(AppDynamicsHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private static void getAndProcessCookies(HttpResponse httpresponse, HttpServletResponse response, Map<String, String> sessions)
+    {
+        String sessionid = null;
+        
+        Pattern patternSessionID = Pattern.compile("^JSESSIONID=([^;]+)");
+        Pattern patternMySQL = Pattern.compile("^MYSQL_host=([^;]+)");
+        Pattern patternPgSql = Pattern.compile("^POSTGRES_host=([^;]+)");
+
+        String mysql_host = null;
+        String pg_host = null;
+        String set_cookie = null;
+
+        // Get all the cookies sent by AppDynamics.
+        Header[] headers = httpresponse.getHeaders("Set-Cookie");
+        for (Header header : headers)
+        {
+            Matcher matcherSessionID = patternSessionID.matcher(header.getValue());
+            if (matcherSessionID.find())
+            {
+                sessionid = matcherSessionID.group(1);
+                // The JSESSIONID has to be sent to the user to trace the request.
+                // However, the name is changed to APPDYNID, to avoid collisions with the JSESSIONID created by Java.
+                set_cookie = header.getValue().replace("JSESSIONID", "APPDYNID");
+            }
+
+            Matcher matcherMySQL = patternMySQL.matcher(header.getValue());
+            if (matcherMySQL.find())
+                mysql_host = matcherMySQL.group(1);
+
+            Matcher matcherPgSql = patternPgSql.matcher(header.getValue());
+            if (matcherPgSql.find())
+                pg_host = matcherPgSql.group(1);
+        }
+
+        // Send the cookie to the client.
+        response.setHeader("Set-Cookie", set_cookie);
+
+        // Build the cookie and save in our sessions map.
+        StringBuilder cookieString = new StringBuilder();
+        cookieString.append("JSESSIONID=").append(sessionid).append(";");
+
+        if (mysql_host != null)
+            cookieString.append("MYSQL_host=").append(mysql_host).append(";");
+
+        if (pg_host != null)
+            cookieString.append("POSTGRES_host=").append(pg_host).append(";");
+
+        sessions.put(sessionid, cookieString.toString());
+    }
+    
+    /**
+     * Get the APPDYNID Cookie.
+     * @param request
+     * @return APPDYNID
+     */
+    private static String getSessionFromCookies(HttpServletRequest request)
+    {
+        String appdynid = null;
+        
+        // Get cookies, if jsessionid arrived I need to proxy it to the server.
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null)
+        {
+            for (Cookie c : cookies)
+            {
+                if (c.getName().equals("APPDYNID"))
+                    appdynid = c.getValue();
+            }
+        }
+        
+        return appdynid;
+    }
+
+    private static int checkSecurityToken(HttpServletRequest request, String appdynid, HttpGet httprequest, Map<String, String> sessions)
+    {
+        String sec_token = request.getParameter("sec_token");
+        String host = request.getParameter("host");
+        
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        String date = dateFormat.format(new Date());
+        
+        // Add cookies if this session already exists
+        if (appdynid != null)
+        {
+            // Check if I have received a security token to change the host
+            // This is needed to replace the cookie if the user has changed to another instance
+            if (sessions.containsKey(appdynid))
+            {
+                // The token is valid, so the request is processed
+                if (host != null && sec_token != null && sec_token.equalsIgnoreCase(DigestUtils.sha256Hex(ConfigLoader.getProperty(CommonConstants.APPDYN_TOKEN) + ":" + host + ":" + date)))
+                {
+                    // Don't send the cookie this time, so a new session and cookie will be created
+                    httprequest.addHeader("Cookie", "anycookie=1;");
+                    return TOKEN_STATUS_NEW;
+                }
+                else
+                {
+                    String sessionhost = sessions.get(appdynid);
+                    
+                    // The user is trying to change the host without a valid token
+                    if (host != null && !sessionhost.endsWith("_host=" + host + ";"))
+                        return TOKEN_STATUS_FAIL;
+                        
+                    // Send our internal data
+                    httprequest.addHeader("Cookie", sessionhost);
+                    return TOKEN_STATUS_OK;
+                }
+            }
+            else
+            {
+                // The session is not saved in our map, check if the token is valid
+                if (host != null && sec_token != null && sec_token.equalsIgnoreCase(DigestUtils.sha256Hex(ConfigLoader.getProperty(CommonConstants.APPDYN_TOKEN) + ":" + host + ":" + date)))
+                {
+                    // Don't send the cookie this time, so a new session and cookie will be created
+                    httprequest.addHeader("Cookie", "anycookie=1;");
+                    return TOKEN_STATUS_NEW;
+                }
+                else
+                {
+                    // The token is invalid, or not provided
+                    return TOKEN_STATUS_FAIL;
+                }
+            }
+        }
+        else 
+        {
+            // The token is valid, so the request is processed
+            if (host != null && sec_token != null && sec_token.equalsIgnoreCase(DigestUtils.sha256Hex(ConfigLoader.getProperty(CommonConstants.APPDYN_TOKEN) + ":" + host + ":" + date)))
+            {
+                // Don't send the cookie this time, so a new session and cookie will be created
+                httprequest.addHeader("Cookie", "anycookie=1;");
+                return TOKEN_STATUS_NEW;
+            }
+            else
+            {
+                return TOKEN_STATUS_FAIL;
+            }
         }
     }
 }
